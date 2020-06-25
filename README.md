8ae37392c49e242b08fa0a39533383669c25c1a6
Meghana Karumuri <meghanakarumuri@gmail.com>
2020-06-25 18:24:45 +0000

    added new files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 2020-summer-t0-speech-recognition
 # 2020-summer-t0-speech-recognition
+# 2020-summer-t0-speech-recognition
---
+++ models.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  8 17:43:51 2018
+@author: chris
+"""
+
+from keras.layers import *
+from keras.layers.wrappers import TimeDistributed
+from keras.layers.merge import Add
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
+from keras import backend as K
+from keras.utils import plot_model
+
+
+#%%
+class ResNet():
+    """
+    Usage: 
+        sr = ResNet([4,8,16], input_size=(50,50,1), output_size=12)
+        sr.build()
+        followed by sr.m.compile(loss='categorical_crossentropy', 
+                                 optimizer='adadelta', metrics=["accuracy"])
+        save plotted model with: 
+            keras.utils.plot_model(sr.m, to_file = '<location>.png', 
+                                   show_shapes=True)
+    """
+    def __init__(self,
+                 filters_list=[], 
+                 input_size=None, 
+                 output_size=None,
+                 initializer='glorot_uniform'):
+        self.filters_list = filters_list
+        self.input_size = input_size
+        self.output_size = output_size
+        self.initializer = initializer
+        self.m = None        
+    
+    def _block(self, filters, inp):
+        """ one residual block in a ResNet
+        
+        Args:
+            filters (int): number of convolutional filters
+            inp (tf.tensor): output from previous layer
+            
+        Returns:
+            tf.tensor: output of residual block
+        """
+        layer_1 = BatchNormalization()(inp)
+        act_1 = Activation('relu')(layer_1)
+        conv_1 = Conv2D(filters, (3,3), 
+                        padding = 'same', 
+                        kernel_initializer = self.initializer)(act_1)
+        layer_2 = BatchNormalization()(conv_1)
+        act_2 = Activation('relu')(layer_2)
+        conv_2 = Conv2D(filters, (3,3), 
+                        padding = 'same', 
+                        kernel_initializer = self.initializer)(act_2)
+        return(conv_2)
+
+    def build(self):
+        """
+        Returns:
+            keras.engine.training.Model
+        """
+        i = Input(shape = self.input_size, name = 'input')
+        x = Conv2D(self.filters_list[0], (3,3), 
+                   padding = 'same', 
+                   kernel_initializer = self.initializer)(i)
+        x = MaxPooling2D(padding = 'same')(x)        
+        x = Add()([self._block(self.filters_list[0], x),x])
+        x = Add()([self._block(self.filters_list[0], x),x])
+        x = Add()([self._block(self.filters_list[0], x),x])
+        if len(self.filters_list) > 1:
+            for filt in self.filters_list[1:]:
+                x = Conv2D(filt, (3,3),
+                           strides = (2,2),
+                           padding = 'same',
+                           activation = 'relu',
+                           kernel_initializer = self.initializer)(x)
+                x = Add()([self._block(filt, x),x])
+                x = Add()([self._block(filt, x),x])
+                x = Add()([self._block(filt, x),x])
+        x = GlobalAveragePooling2D()(x)
+        x = Dense(self.output_size, activation = 'softmax')(x)
+        
+        self.m = Model(i,x)
+        return self.m
+           
+    
+#%%
+def ctc_lambda_func(args):
+    y_pred, labels, input_length, label_length = args    
+    return K.ctc_batch_cost(labels, y_pred, input_length, label_length)    
+    
+        
+#%%
+class CTC():
+    """
+    Usage:
+        sr_ctc = CTC(enter input_size and output_size)
+        sr_ctc.build()
+        sr_ctc.m.compile()
+        sr_ctc.tm.compile()
+    """       
+    def __init__(self,
+                 input_size=None, 
+                 output_size=None,
+                 initializer='glorot_uniform'):
+        self.input_size = input_size
+        self.output_size = output_size
+        self.initializer = initializer
+        self.m = None
+        self.tm = None
+                   
+    def build(self, 
+              conv_filters = 196,
+              conv_size = 13,
+              conv_strides = 4,
+              act = 'relu',
+              rnn_layers = 2,
+              LSTM_units = 128,
+              drop_out = 0.8):
+        """
+        build CTC training model (self.m) and 
+        prediction model without the ctc loss function (self.tm)
+        
+        Usage: 
+            enter conv parameters for Cov1D layer
+            specify number of rnn layers, LSTM units and dropout
+        Args:
+            
+        Returns:
+            self.m: keras.engine.training.Model
+            self.tm: keras.engine.training.Model
+        """        
+        i = Input(shape = self.input_size, name = 'input')
+        x = Conv1D(conv_filters, 
+                   conv_size, 
+                   strides = conv_strides, 
+                   name = 'conv1d')(i)
+        x = BatchNormalization()(x)
+        x = Activation(act)(x)
+        for _ in range(rnn_layers):          
+            x = Bidirectional(LSTM(LSTM_units, 
+                                   return_sequences = True))(x)
+            x = Dropout(drop_out)(x)
+            x = BatchNormalization()(x)
+        y_pred = TimeDistributed(Dense(self.output_size, 
+                                       activation = 'softmax'))(x)        
+        # ctc inputs
+        labels = Input(name='the_labels', shape=[None,], dtype='int32')
+        input_length = Input(name='input_length', shape=[1], dtype='int32')
+        label_length = Input(name='label_length', shape=[1], dtype='int32')    
+        # Keras doesn't currently support loss funcs with extra parameters
+        # so CTC loss is implemented in a lambda layer
+        loss_out = Lambda(ctc_lambda_func, 
+                          output_shape=(1,), 
+                          name='ctc')([y_pred,
+                                        labels,
+                                        input_length,
+                                        label_length])        
+        self.tm = Model(inputs = i,
+                        outputs = y_pred)
+        self.m = Model(inputs = [i, 
+                                 labels, 
+                                 input_length, 
+                                 label_length], 
+                        outputs = loss_out)
+        return self.m, self.tm
